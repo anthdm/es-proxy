7557691360cff2ea08ce6087329d2d7e94f8770c
Anthony <anthonydm@bubblepost.be>
2017-10-26 16:49:02 +0200

    Based on NewrelicId for now.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 )
 
 func main() {
@@ -13,20 +14,27 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	id := os.Getenv("NEW_RELIC_ID")
 
-	log.Fatal(http.ListenAndServe(":3000", NewEsProxy(url)))
+	log.Fatal(http.ListenAndServe(":3000", NewEsProxy(url, id)))
 }
 
 type EsProxy struct {
-	p *httputil.ReverseProxy
+	p          *httputil.ReverseProxy
+	newRelicID string
 }
 
-func NewEsProxy(target *url.URL) *EsProxy {
-	return &EsProxy{httputil.NewSingleHostReverseProxy(target)}
+func NewEsProxy(target *url.URL, id string) *EsProxy {
+	return &EsProxy{
+		p:          httputil.NewSingleHostReverseProxy(target),
+		newRelicID: id,
+	}
 }
 
 func (proxy *EsProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.RemoteAddr)
-	log.Println(r.Header)
-	proxy.p.ServeHTTP(w, r)
+	if len(r.Header["X-Newrelic-Id"]) > 0 && r.Header["X-Newrelic-Id"][0] == proxy.newRelicID {
+		proxy.p.ServeHTTP(w, r)
+		return
+	}
+	http.Error(w, "Not authorized", 403)
 }
